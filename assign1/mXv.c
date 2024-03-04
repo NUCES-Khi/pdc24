9e2a101429d0b12a0194f302ce9b5a78a83e3035
Farooq Zaidi <123715058+farooqNU@users.noreply.github.com>
2024-03-04 11:41:08 +0500

    Create mXv.c

---
+++ assign1/mXv.c
@@ -0,0 +1,12 @@
+/*
+ * Programmer(s) : List all those who contributed in this file
+ * Date: 
+ * Desc: Sequential version of matrix vector multiplication. 
+ */
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(int argc, char* argv){
+  //enter you code here...
+}//end main
