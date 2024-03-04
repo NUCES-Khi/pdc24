415dadcb88a8ea906f2dd5f43d12d611c5073da0
Farooq Zaidi <123715058+farooqNU@users.noreply.github.com>
2024-03-04 11:42:47 +0500

    Update mXv.c

--- assign1/mXv.c
+++ assign1/mXv.c
@@ -2,6 +2,7 @@
  * Programmer(s) : List all those who contributed in this file
  * Date: 
  * Desc: Sequential version of matrix vector multiplication. 
+ * Note: you will have to make similar files for other 4 version as explained in assign1.md and a batch file to get running times of all 5 programs.
  */
 #include<stdio.h>
 #include<stdlib.h>
